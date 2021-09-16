3d7b80c84f9d4266dcc1fb9591aae50a496b63d9
StedHouston <s.houston406@gmail.com>
2021-09-16 16:24:19 -0500

    added database tables

---
+++ src/main/resources/tweeter_tables.sql
@@ -0,0 +1,25 @@
+create table app_user (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    created_at DATE NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    handle VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL
+);
+
+create table tweet (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    app_user_id BIGINT REFERENCES app_user (id) NOT NULL,
+    message VARCHAR(124) NOT NULL,
+    retweet BOOL NOT NULL,
+    retweet_author_id BIGSERIAL,
+    created_at DATE NOT NULL,
+    UNIQUE(app_user_id)
+);
+
+create table user_followers (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    user_id BIGINT REFERENCES  app_user (id) NOT NULL,
+    follower_id BIGINT REFERENCES  app_user (id) NOT NULL
+);
